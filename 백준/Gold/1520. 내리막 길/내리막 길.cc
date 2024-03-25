68a0754c880792a9b834c3769c69dc2be14d6cca
Seunggwang ROH (Leo) <40450463+seung-gwang@users.noreply.github.com>
2024-03-26 00:03:04 +0900

    [Gold III] Title: 내리막 길, Time: 100 ms, Memory: 4216 KB -BaekjoonHub

---
+++ 백준/Gold/1520. 내리막 길/내리막 길.cc
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+int map[500][500];
+int M;//세로
+int N;//가로
+int dy[] = { -1,1,0,0 };
+int dx[] = { 0,0,-1,1 };
+
+bool visit[500][500];
+int dp[500][500];
+
+int routeCnt;
+
+int DFS(const int cy, const int cx) {
+
+    if (dp[cy][cx] != -1) {
+        return dp[cy][cx];
+    }
+
+    
+
+    if (cy == M - 1 && cx == N - 1) {
+        dp[cy][cx] = 1;
+        return 1;
+    }
+
+    dp[cy][cx] = 0;
+
+    int curHeight = map[cy][cx];
+
+    for (int dir = 0; dir < 4; ++dir) {
+        int ny = cy + dy[dir];
+        int nx = cx + dx[dir];
+
+        if (ny < 0 || nx < 0 || ny >= M || nx >= N) {
+            continue;
+        }
+
+        if (map[ny][nx] >= curHeight) {
+            continue;
+        }
+
+        if (visit[ny][nx]) {
+            continue;
+        }
+
+        visit[ny][nx] = true;
+        dp[cy][cx] += DFS(ny, nx);
+        visit[ny][nx] = false;
+    }
+
+    return dp[cy][cx];
+}
+
+void solve() {
+    routeCnt = 0;
+    cin >> M >> N;
+    for (int y = 0; y < M; ++y) {
+        for (int x = 0; x < N; ++x) {
+            cin >> map[y][x];
+            visit[y][x] = false;
+            dp[y][x] = -1;
+        }
+    }
+
+    visit[0][0] = true;
+    cout << DFS(0, 0);
+
+    
+}
+
+int main() {
+    solve();
+    return 0;
+}
