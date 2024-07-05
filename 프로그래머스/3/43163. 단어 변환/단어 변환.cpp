08cd16df62d8f5b8f85920aba3ad6f289de1470d
Seunggwang ROH (Leo) <40450463+seung-gwang@users.noreply.github.com>
2024-07-05 23:06:48 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,55 +1,68 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
+#define INF (999999999)
 
-string g_target;
-vector<string> g_words;
-bool v[51];
+int gAnswer = INF;
+string gTarget = "";
+vector<string> gWords;
+bool visit[50];
 
-int DFS(string cur, int cnt);
-
-int diffCnt(string a, string b);
-
-int solution(string begin, string target, vector<string> words) {
-    for(int i = 0; i < words.size(); ++i) {
-        v[i] = false;
-    }
-    int answer = 0;
-    g_target = target;
-    g_words = words;
-    
-    if(find(words.begin(), words.end(), target) == words.end()) {
-        return 0;
-    }
-    
-    return DFS(begin, 0);
-}
-
-int DFS(string cur, int cnt) {
-    if(cur == g_target) {
-        return cnt;
-    }
-    
-    int rec = -1;
-    for(int i = 0; i < g_words.size(); ++i) {
-        if(diffCnt(cur, g_words[i]) == 1 && v[i] == false) {
-            v[i] = true;
-            rec = DFS(g_words[i], cnt+1);
-            v[i] = false;
+bool isTransferable(const string a, const string b) {
+    int diff = 0;
+    const int LEN = a.length();
+    for(int i = 0; i < LEN; ++i) {
+        if(a[i] != b[i]) {
+            diff++;
+        }
+        
+        if(diff > 1) {
+            return false;
         }
     }
     
-    return rec;
+    return true;
 }
 
-int diffCnt(const string a, const string b) {
-    int diff = 0;
-    const int len = a.length();
-    for(int i = 0; i < len; ++i) {
-        if(a[i] != b[i]) {diff++;}
+void DFS(const string cur, const int step) {
+    if(cur == gTarget) {
+        gAnswer = step > gAnswer ? gAnswer : step;
+        return;
     }
     
-    return diff;
+    const int LEN = gWords.size();
+    for(int i = 0; i < LEN; ++i) {
+        const string w = gWords[i];
+        
+        if(visit[i]) {
+            continue;
+        }
+        
+        if(w == cur) {
+            continue;
+        }
+        
+        if(isTransferable(cur, w)) {
+            visit[i] = true;
+            DFS(w, step+1);
+            visit[i] = false;
+        }
+    }
+    
+}
+
+int solution(string begin, string target, vector<string> words) {
+    int answer = 0;
+    gTarget = target;
+    gWords = words;
+    
+    DFS(begin, 0);
+    
+    answer = gAnswer;
+    if(answer == INF) {
+        answer = 0;
+    }
+    
+    return answer;
 }
