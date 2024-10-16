60e3a2e0fb4ce1c86e8b093ed815813788a28b50
Seunggwang ROH (Leo) <40450463+seung-gwang@users.noreply.github.com>
2024-10-16 19:28:41 +0900

    [level 2] Title: [1차] 캐시, Time: 132.40 ms, Memory: 114 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -0,0 +1,49 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.LinkedList;
+
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        
+        HashMap<String, Integer> cache = new HashMap<>(); //도시명-캐싱시간
+        
+        
+        int curTime = 0;
+        for(String city : cities) {
+            String c = city.toLowerCase();
+            //cache Hit
+            if(cache.containsKey(c)) {
+                cache.put(c, curTime);
+                curTime+=1;
+                continue;
+            }
+            
+            //cache Miss
+            if(cache.size() == cacheSize) {//퇴거
+                
+                String lru = null;
+                int minTime = Integer.MAX_VALUE;
+                for(String k : cache.keySet()) {
+                    int v = cache.get(k);
+                    if(v < minTime) {
+                        minTime = v;
+                        lru = k;
+                    }
+                }
+                cache.remove(lru);
+            }
+            
+            if(cache.size() < cacheSize) {
+                cache.put(c, curTime);    
+            }
+            
+            curTime+=5;
+        }
+        
+        answer = curTime;
+        
+        return answer;
+    }
+}
